c31ea8705aa4489291a1902a9120420f6ea7208e
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2022-11-21 20:17:21 +0000

    fix error handling for api

--- src/components/Pages/CoinDetails.js
+++ src/components/Pages/CoinDetails.js
@@ -1,30 +1,83 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  getSingleCoinFromApi,
+  initialState,
+} from '../../redux/singleCryptoCoin/singleCryptoCoin';
 
-const CoinDetails = () => (
-  <div className="coin-container flex">
-    <h1>Ethereum</h1>
-    <p>eth</p>
-    <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" alt="coin imgae" />
-    <div>
-      <h3>Relevant Stats</h3>
-      <p>Hashing Algorithm: 4565</p>
-      <p>Market Cap Rank: 3</p>
-      <p>Total Volume: 23344533</p>
-      <div>
-        <h3>Daily Price</h3>
-        <p>High 24: 23</p>
-        <p>Low 24: 5</p>
-      </div>
-      <h3>Price Changes</h3>
-      <div>
-        <p>24h: 33</p>
-        <p>7d: 33</p>
-        <p>14d: 56</p>
-      </div>
+const CoinDetails = () => {
+  const coin = useSelector((state) => state.singleCoin);
+  const id = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getSingleCoinFromApi(id.id));
+  }, []);
+
+  return (
+    <div className="coin-container flex">
+      <h1>{coin.name}</h1>
+      <p>{coin.symbol}</p>
+      {coin === initialState ? (
+        <p>Loading</p>
+      ) : (
+        <div>
+          <img src={coin.image.large} alt="coin img" />
+          <div>
+            <h3>Relevant Stats</h3>
+            <p>
+              Categories:
+              {' '}
+              {coin.categories[0]}
+            </p>
+            <p>
+              Market Cap Rank:
+              {' '}
+              {coin.market_cap_rank}
+            </p>
+            <p>
+              CMC Rank:
+              {' '}
+              {coin.coingecko_rank}
+              {' '}
+            </p>
+            <div>
+              <h3>Daily Price</h3>
+              <p>
+                High 24h: $
+                {`${coin.market_data.high_24h.usd}`}
+              </p>
+              <p>
+                Low 24h: $
+                {`${coin.market_data.low_24h.usd}`}
+              </p>
+            </div>
+            <h3>Price Changes</h3>
+            <div>
+              <p>
+                1hr: %
+                {coin.market_data.price_change_percentage_1h_in_currency.usd}
+              </p>
+              <p>
+                24h: %
+                {coin.market_data.price_change_percentage_24h}
+              </p>
+              <p>
+                30d: %
+                {coin.market_data.price_change_percentage_30d}
+              </p>
+              <p>
+                60d: %
+                {' '}
+                {coin.market_data.price_change_percentage_60d}
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-
-  </div>
-
-);
-
+  );
+};
 export default CoinDetails;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import CoinReducer from './allCryptoCoins/allCryptoCoins';
+import SingleCoinReducer from './singleCryptoCoin/singleCryptoCoin';
 
 const store = configureStore({
   reducer: {
     coins: CoinReducer,
-
+    singleCoin: SingleCoinReducer,
   },
 });
 
